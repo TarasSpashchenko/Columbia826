b1570b9c1d2293ab7a99ed4f4693f124630648be
oleksii_kuzminov <oleksii_kuzminov@epam.com>
2019-04-09 12:36:52 +0300

    event processing

--- core/src/main/java/com/folijet/columbia/core/processing/EventProcessor.java
+++ core/src/main/java/com/folijet/columbia/core/processing/EventProcessor.java
@@ -2,9 +2,20 @@ package com.folijet.columbia.core.processing;
 
 import com.folijet.columbia.core.metadata.EventPayload;
 
+/**
+ * Common entry point for start listening and processing messages
+ */
 public interface EventProcessor {
 
+    /**
+     * Call this method to start listening messages and consumes them
+     */
     void startEventProcessing();
 
+    /**
+     * Finish event processing and send next message to the query
+     *
+     * @param eventPayload - prepared event payload to send
+     */
     void finishEventProcessing(EventPayload eventPayload);
 }
--- core/src/main/java/com/folijet/columbia/core/processing/JobEventHandler.java
+++ core/src/main/java/com/folijet/columbia/core/processing/JobEventHandler.java
@@ -5,9 +5,17 @@ import io.vertx.core.Vertx;
 
 public interface JobEventHandler {
 
+    // event type like "ACTION", "MATCH", "ERROR", etc.
     String getEventType();
 
+    // entity type like "INSTANCE", "HOLDINGS", etc.
     String getEntityType();
 
+    /**
+     * Process received event message
+     * @param vertx - vetx object
+     * @param eventPayload - received event message
+     * @param eventProcessor - eventProcessor for calling finish event logic
+     */
     void processEvent(Vertx vertx, EventPayload eventPayload, EventProcessor eventProcessor);
 }
